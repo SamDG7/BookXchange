708bf9a8ef7c3530a2b44ab4b3b891f35982ee34
monroee23 <82832396+monroee23@users.noreply.github.com>
2023-10-13 23:08:31 -0400

    Added 'remove book from library' button

--- bookxchange_flutter/lib/screens/book_page.dart
+++ bookxchange_flutter/lib/screens/book_page.dart
@@ -34,8 +34,6 @@ class _BookAboutScreenState extends State<BookAboutScreen> {
             padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
             child: Column(
               children: [
-
-
                 // FutureBuilder<ExistingUser>(
                 //           // pass the list (postsFuture)
                 //           future: _existingUser,
@@ -54,25 +52,6 @@ class _BookAboutScreenState extends State<BookAboutScreen> {
                 //               // we did not recieve any data, maybe show error or no data available
                 //             }
                 //           }),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
                 Text(
                   "Watership Down",
@@ -105,127 +84,135 @@ class _BookAboutScreenState extends State<BookAboutScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                OutlinedButton.icon(
-                  onPressed: () {
-                    //Navigator.push(
-                    //    context,
-                    //    MaterialPageRoute(
-                    //         builder: (context) => BookAboutScreen()),
-                    // add screen to edit library here
-                    //     );
-                  },
-                  icon: Icon(
-                    Icons.read_more,
-                    color: butterfly,
-                  ),
-                  label: Text(
-                    'See More',
-                    style: TextStyle(color: Colors.grey[800]),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    side: BorderSide(width: 1.0, color: butterfly),
-                  ),
-                ),
-                
-                OutlinedButton.icon(
-                  onPressed: () {
-                    //Navigator.push(
-                    //    context,
-                    //    MaterialPageRoute(
-                    //         builder: (context) => BookAboutScreen()),
-                    // add screen to edit library here
-                    //     );
-                  },
-                  icon: Icon(
-                    Icons.edit,
-                    color: butterfly,
-                  ),
-                  label: Text(
-                    'Edit Book',
-                    style: TextStyle(color: Colors.grey[800]),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    side: BorderSide(width: 1.0, color: butterfly),
-                  ),
-                ),
+                    OutlinedButton.icon(
+                      onPressed: () {
+                        //Navigator.push(
+                        //    context,
+                        //    MaterialPageRoute(
+                        //         builder: (context) => BookAboutScreen()),
+                        // add screen to edit library here
+                        //     );
+                      },
+                      icon: Icon(
+                        Icons.read_more,
+                        color: butterfly,
+                      ),
+                      label: Text(
+                        'See More',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(width: 1.0, color: butterfly),
+                      ),
+                    ),
+                    OutlinedButton.icon(
+                      onPressed: () {
+                        //Navigator.push(
+                        //    context,
+                        //    MaterialPageRoute(
+                        //         builder: (context) => BookAboutScreen()),
+                        // add screen to edit library here
+                        //     );
+                      },
+                      icon: Icon(
+                        Icons.edit,
+                        color: butterfly,
+                      ),
+                      label: Text(
+                        'Edit Book',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(width: 1.0, color: butterfly),
+                      ),
+                    ),
                   ],
                 ),
-               
               ],
             ),
           ),
-           Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-                  
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Statistics",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-
-                      SizedBox(height: 10),
-
-                      Text(
-                        "Status:", //swapped or in library
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-
-
-
-
-                      Text(
-                        "Number of Swaps:",
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-
-                      Text(
-                        "Time on Profile:",
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-
-                      SizedBox(height: 10),
-
-                      Text(
-                        "Your Review:",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-
-                      SizedBox(height: 5),
-
-                      Text(
-                        " 'Hi I thought this book was absolutely awesome! My dad read it to me and my siblings when we were little and rereading it now has been just as fun as the first time.'  🐰🐰🤼‍♀️\n- Elena ",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-
-
-
-
-
-                    ],
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Statistics",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
+                SizedBox(height: 10),
+                Text(
+                  "Status:", //swapped or in library
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Number of Swaps:",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Time on Profile:",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Your Review:",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  " 'Hi I thought this book was absolutely awesome! My dad read it to me and my siblings when we were little and rereading it now has been just as fun as the first time.'  🐰🐰🤼‍♀️\n- Elena ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [ Padding(
+                      padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      child: OutlinedButton.icon(
+                      onPressed: () {
+                        //Navigator.push(
+                        //    context,
+                        //    MaterialPageRoute(
+                        //         builder: (context) => BookAboutScreen()),
+                        // add screen to edit library here
+                        //     );
+                      },
+                      icon: Icon(
+                        Icons.remove,
+                        color: butterfly,
+                      ),
+                      label: Text(
+                        'Remove book from library',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(width: 1.0, color: butterfly),
+                      ),
+                    ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
-
 
   Widget buildTitle(String title) {
     return Text(
@@ -238,5 +225,4 @@ class _BookAboutScreenState extends State<BookAboutScreen> {
       ),
     );
   }
-
 }
