e0a92199bd5ebdb77acf59868fea62aee8c317dc
SamDG7 <samdeluciagreen@gmail.com>
2023-10-28 14:18:40 -0400

    Created Queue Class

--- bookxchange_flutter/lib/api/library_profile.dart
+++ bookxchange_flutter/lib/api/library_profile.dart
@@ -1,25 +1,21 @@
 import 'package:bookxchange_flutter/api/book_profile.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:http_interceptor/http_interceptor.dart';
 import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
 
 Future<Library> getCurrentLibrary(String uuid) async {
+  final queryParameters = {'uuid': uuid};
 
-  final queryParameters = {
-    'uuid': uuid
-  };
-
-  final response = await http
-
-    .get(Uri.parse('http://127.0.0.1:8080/library/''$uuid'));
+  final response =
+      await http.get(Uri.parse('http://127.0.0.1:8080/library/' '$uuid'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Library.fromJson(jsonDecode(response.body)[0] as Map<String, dynamic>);
+    return Library.fromJson(
+        jsonDecode(response.body)[0] as Map<String, dynamic>);
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
@@ -27,21 +23,16 @@ Future<Library> getCurrentLibrary(String uuid) async {
   }
 }
 
-
-
 class Library {
-  
   final String uuid;
   final List<Book> bookList;
-  
+
   const Library({required this.uuid, required this.bookList});
 
   factory Library.fromJson(Map<String, dynamic> json) {
     return Library(
-
       uuid: json['uuid'],
       bookList: json['book_list'],
-     
     );
   }
 }
