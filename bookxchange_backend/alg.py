ce261d9a0833b837b0e38a263f86b66963872fd3
SamDG7 <samdeluciagreen@gmail.com>
2023-10-31 12:23:48 -0400

    basic swiping func

--- bookxchange_backend/alg.py
+++ bookxchange_backend/alg.py
@@ -14,10 +14,7 @@ import requests
 from flask_cors import CORS, cross_origin
 from requests_toolbelt.multipart import decoder
 
-
-def calcDistance(pref, book_pref):
-
-  encode = {'Fantasy': 0, 
+encode = {'Fantasy': 0, 
             'Young-adult': 1,
             'Romance': 2,
             'New-adult': 3,
@@ -28,14 +25,16 @@ def calcDistance(pref, book_pref):
             'Crime and Mystery': 8, 
             'Biography': 9, 
             'Historical': 10}
-  
+
+def calcDistance(pref, book_pref):
   score = 0
   print(f'Comparing books with bp {book_pref} and p {pref}')
  
   for bp in book_pref:
     if bp not in pref and bp in encode:
       for p in pref:
-        score += abs(encode[bp] - encode[p])
+        if p in encode:
+          score += abs(encode[bp] - encode[p])
   
   return score
 
@@ -43,17 +42,11 @@ def calcDistance(pref, book_pref):
 def createQueue(uuid, collection, preferences):
   queue = []
   user = list(db.db.user_collection.find({"uuid": uuid}))
-
-  print(f'THIS IS THE USER:::: {user}')
-  print(f'PREFERENCESEFSE:::{preferences}')
-
   for doc in collection:
     if doc['uuid'] != uuid:
       current_book_genres = doc['genres']
       score = calcDistance(preferences, current_book_genres)
-      print((doc['_id'], score))
       queue.append([doc['_id'], score])
-      print(doc['uuid'])
 
   queue.sort(key= lambda x: x[1])
 
@@ -61,30 +54,59 @@ def createQueue(uuid, collection, preferences):
 
   return queue
 
+
+
+
+
 #right is a boolean, book_id is the object ID of the book swiped on
-def updateQueueOnSwipe(uuid, book_id, right):
-  queue = list(db.db.queue_collection.find({"uuid": uuid}))
-  book = list(db.db.book_collection.find({"_id": book_id}))
+def updateQueueOnSwipe(uuid, right):
+  queue = list(db.db.queue_collection.find({"uuid": uuid}))[0]['queue']
+  swiped_book = list(db.db.book_collection.find({'_id': queue[0][0]}))
+  print(f'This is the users current queue: {queue}\n\n\n\n')
+  queue.pop(0)
 
-  book_genres = book['genres']
-  if right:
-    for c, b in enumerate(queue):
-      #move books similar to the one swiped on up in the queue, except for the first 5 books remaing constant
-      if c > 5:
-        iterative_book = list(db.db.book_collection.find({"_id": b[0]}))
-        book_score = b[1];
-        overlap = sum(el in iterative_book['genres'] for el in book_genres)
+  if right:    
+    swiped_book_genres = swiped_book[0]['genres']
 
-        if overlap > 0:
-          #decrease by 2
-          ...
-        elif overlap > 2:
-          #decrease by 4
-          ...
-      ...
+    for book in queue:
+      curr_book = list(db.db.book_collection.find({'_id': book[0]}))[0]
+      curr_book_genres = curr_book['genres']
+      val = calcDistance(swiped_book_genres, curr_book_genres)
+      if val == 0:
+        book[1] -= 5
+      elif val < 5:
+        book[1] -=2
+      elif val < 10:
+        book[1] -=1
+   
+    queue.sort(key= lambda x: x[1])
+    print(queue)
+    #mark as wanted
   else:
-    #move similar books down in queue, except for the first five docts
-    ...
+    swiped_book[1] += 30
 
+  
 
+  #add 1 new book to queue
+    
+  if not right:
+    queue.append(swiped_book)
+
+  queue.sort(key= lambda x: x[1])
+  db.db.queue_collection.find_one_and_replace({'uuid': uuid}, {'$set': {
+      'queue': queue
+    }})
   return 1
+
+
+
+
+
+
+
+
+def main():
+  updateQueueOnSwipe('ds3nim2RO6MuNWkngeTRclFNfcZ2', True)
+  return 0
+if __name__ == "__main__":
+    main()
