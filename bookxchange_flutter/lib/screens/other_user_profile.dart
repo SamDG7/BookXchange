a5546036e085a9f55e2cca1267ea08d7fa8489af
monroee23 <82832396+monroee23@users.noreply.github.com>
2023-12-01 12:56:02 -0500

    Edited text style

--- bookxchange_flutter/lib/screens/other_user_profile.dart
+++ bookxchange_flutter/lib/screens/other_user_profile.dart
@@ -139,7 +139,7 @@ class _OtherUserState extends State<OtherUser> {
                                 Text(
                                   aboutMeData,
                                   style: TextStyle(
-                                    fontSize: 20,
+                                    fontSize: 18,
                                     fontWeight: FontWeight.bold,
                                     color: butterfly,
                                   ),
